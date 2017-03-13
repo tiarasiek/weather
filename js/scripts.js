99ab0deedc901564a18f870a68cc3d45895fa9ca
tiarasiek <tsiek@vcd-ch204-22.eastern.ewu.edu>
2017-03-13 14:14:13 -0700

    Added weather js
    *Added simpleWeather plugin
    *Added css
    *Added html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
 // YOUR SCRIPTS GO HERE
+$.simpleWeather({
+  location: 85214,
+  
+  unit: 'f',
+  
+  success: function(weather){
+    // Check for weather
+    console.log(weather);
+    
+    // Display Weather
+    $('.temp').text(weather.temp);
+    $('.city').text(weather.city);
+    $('.state').text(weather.region);
+    $('.image img').attr('src', weather.image);
+    
+  },
+  
+  error: function(error) {
+    console.log('Look outside.');
+  }
+});
