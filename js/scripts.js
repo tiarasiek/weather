0ac169c42a7d670daacbd182d0c6085c05e361fb
tiarasiek <User@Users-MacBook-Pro.local>
2017-03-22 13:36:18 -0700

    Basic styling, geolocation
    *Added some base styling
    *Added Geolocation option button
    *Added temp. gradients to change depending on weather code

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,75 @@
 // YOUR SCRIPTS GO HERE
-$.simpleWeather({
-  location: 85214,
+
+// Get Geo Location
+// Check for GeoLocation Support on Browser
+if ('geolocation' in navigator) {
+
+   $('.geo').show(); 
+
+} else {
   
-  unit: 'f',
-  
-  success: function(weather){
-    // Check for weather
-    console.log(weather);
-    
-    // Display Weather
-    $('.temp').text(weather.temp);
-    $('.city').text(weather.city);
-    $('.state').text(weather.region);
-    $('.image img').attr('src', weather.image);
-    
-  },
-  
-  error: function(error) {
-    console.log('Look outside.');
-  }
+  $('.geo').hide();
+  $('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('.geo').click( function() {
+      
+    //load weather using your lat/long coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+      
+      // Check lat/long coordinates
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      
+      console.log(lat, long);
+      
+      // Send to SimpleWeather
+      getWeather( lat + ',' + long ); 
+    });
+   
 });
+
+// Show geolocated weather
+var getWeather = function(location) {
+    
+   $.simpleWeather({
+    location: location,
+    unit: 'f',
+    success: function(weather) {
+      
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('img').attr('src', weather.image);
+
+              console.log(weather.code);
+      
+      if ( weather.code >= 23 && weather.code <= 29 ) {
+         $('body').addClass('cloudy');   
+      }
+      
+      
+      if ( weather.code >= 30 && weather.code <= 36 ) {
+         $('body').addClass('sunny');   
+      }
+          
+    if (weather.code >= 5 && weather.code <= 14 ) {
+        
+        $('body').addClass('rainy');
+        
+      } 
+
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look Outside.');
+    }
+  
+  });
+    
+};
